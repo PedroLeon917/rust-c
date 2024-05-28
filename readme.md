889c7a1643f3b540696f5e9cd5b70669ccc4ba49
PedroLeon917 <pedro.leon@everymundo.com>
2024-05-28 11:04:32 -0400

    Update main and readme from Caleb

---
+++ readme.md
@@ -0,0 +1,5 @@
+```
+mkdir lib
+gcc -c -o src/linked-list.o src/linked-list.c;
+ar rcs lib/liblinked-list.a linked-list.o
+```
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
-pub enum LinkedList {} 
+#[repr(C)]
+pub struct Position {
+    x: i32,
+    y: i32,
+}
+
+// Since `LinkedList` are opaque types (we don't need their
+// internal structure in Rust), we represent them as empty enums.
+// These types will only be manipulated through pointers.
+pub enum LinkedList {}
 
 extern "C" {
     fn generateLinkedList() -> *mut LinkedList;
+    fn pushBack(linkedList: *mut LinkedList, x: i32, y: i32);
+    fn printList(linkedList: *mut LinkedList);
+    fn freeList(linkedList: *mut LinkedList);
+    fn get(LinkedList: *mut LinkedList, index: i32) -> Position;
+    fn searchList(LinkedList: *mut LinkedList, x: i32, y: i32) -> *mut Position;
 }
-
 fn main() {
     let list;
     unsafe {
         list = generateLinkedList();
+        pushBack(list, 5, 10);
+        pushBack(list, 10, 15);
+        pushBack(list, 20, 35);
+        printList(list);
+        let pos = get(list, 2);
+        println!("pos: ({}, {})", pos.x, pos.y);
+
+        let pos2 = searchList(list, 20, 35);
+        println!("pos: ({}, {})", (*pos2).x, (*pos2).y);
+
+        if let Some(pos3) = searchList(list, 200, 35).as_ref() {
+            println!("pos: ({}, {})", (*pos3).x, (*pos3).y);
+        }
+
+        freeList(list);
     }
 }
